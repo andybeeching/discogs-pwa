4a940b8f85f464877fddd265a39158dd6dd831b6
Andrew Beeching <andrew@discern.in>
2019-09-05 10:00:52 +0200

    Add basic screens

---
+++ src/lib/urls.js
@@ -0,0 +1,21 @@
+// TODO: Replace with native URL object usage.
+// (This isn't available in node v6, used by Firebase Cloud Functions.)
+// Authentication via headers
+
+const PREFIX = 'https://api.discogs.com'
+
+export function getArtistSearch(query) {
+  return `${PREFIX}/database/search?q=${encodeURIComponent(query)}&type=artist`
+}
+
+export function getArtist(artistId) {
+  return `${PREFIX}/artists/${artistId}`
+}
+
+export function getArtistReleases(artistId) {
+  return `${PREFIX}/artists/${artistId}/releases`
+}
+
+export function getRelease(releaseId) {
+  return `${PREFIX}/release/${releaseId}`
+}
