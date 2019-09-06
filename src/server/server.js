c61c21fa31a5d6306c79512ce92026c0794a01d6
Andrew Beeching <andrew@discern.in>
2019-09-06 16:40:00 +0200

    Refactor out apiclient

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,5 @@
 import express from 'express'
-import fetch from 'node-fetch'
-import LRU from 'lru-cache'
-import * as creds from './../lib/creds'
+import requestData from '../lib/apiClient.js'
 import * as templates from '../lib/templates.js'
 import * as urls from '../lib/urls.js'
 
@@ -13,16 +11,6 @@ import config from '../../webpack.config'
 import head from 'raw-loader!../partials/head.html'
 import nav from 'raw-loader!../partials/nav.html'
 import foot from 'raw-loader!../partials/foot.html'
-
-// Shelter the Discogs endpoints and help respect their rate limit with a
-// light-weight cache on this server process.
-//
-// Ordinarily this would be implemented as a reverse proxy which could stream
-// the HTML response.
-const apiCache = new LRU({
-  max: 100,
-  maxAge: 1000 * 60 * 5 // 5 minutes.
-})
 
 const app = express()
 
@@ -40,35 +28,6 @@ app.use(
 )
 
 app.use(webpackHotMiddleware(compiler))
-
-// basic API client
-async function apiClient(url) {
-  const cachedRes = apiCache.get(url)
-
-  if (cachedRes) {
-    console.log('\x1b[33m%s\x1b[0m', 'LRU CACHE HIT')
-    return cachedRes
-  }
-
-  const networkRes = await fetch(url, {
-    method: 'get',
-    headers: {
-      Authorization: `Discogs key=${creds.KEY}, secret=${creds.SECRET}`
-    }
-  })
-
-  const data = await networkRes.json()
-  apiCache.set(url, data)
-
-  // console.log('\x1b[33m%s\x1b[0m', JSON.stringify(data))
-  return data
-}
-
-// TODO: Implement cache lookup
-async function requestData(url) {
-  const res = await apiClient(url)
-  return res
-}
 
 app.get('/', async (req, res) => {
   res.write(head + nav)
