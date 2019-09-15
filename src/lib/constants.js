2f55e1c71960021f846d8ecac7abfba41b894f6d
Andrew Beeching <andrew@discern.in>
2019-09-15 23:29:24 +0200

    Add tests for DOM Loader

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -2,3 +2,5 @@ export const LOADER_SELECTOR = '.loader'
 export const LOADER_HIDDEN = 'is-hidden'
 export const LOADER_LOADED = 'is-loaded'
 export const LOADER_SHOWN = 'is-shown'
+// Nielsen lower-bound for computer 'working'
+export const LOADER_DELAY = 200
