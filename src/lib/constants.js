6545ce776a318224bf8e0b2a4792a5c3fb1069d6
Andrew Beeching <andrew@discern.in>
2019-09-19 14:31:49 +0200

    :sparkles: feat(UI): Add offline toast notification

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,6 +1,10 @@
 export const LOADER_SELECTOR = '.loader'
-export const LOADER_HIDDEN = 'is-hidden'
-export const LOADER_LOADED = 'is-loaded'
-export const LOADER_SHOWN = 'is-shown'
 // Nielsen lower-bound for computer 'working'
 export const LOADER_DELAY = 200
+
+export const TOAST_SELECTOR = '.toast-msg'
+export const TOAST_DURATION = 3000
+
+export const IS_HIDDEN = 'is-hidden'
+export const IS_LOADED = 'is-loaded'
+export const IS_SHOWN = 'is-shown'
